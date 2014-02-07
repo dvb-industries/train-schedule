bcb5a93a8e182d3117fea37581df322fa28c63ef
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2014-02-07 10:38:13 +0100

    create a skeletal schedule view

--- public/js/schedule.js
+++ public/js/schedule.js
@@ -1,4 +1,19 @@
 (function(io){
+    var ScheduleView = function(parent) {
+	this.parent = parent;
+	this.update();
+    };
+    ScheduleView.prototype.update = function(){
+	var container = this.container();
+	container.innerHTML = 'Hello World';
+
+    };
+    ScheduleView.prototype.container = function(){
+	return this.parent;
+    };
+
+    new ScheduleView(document.getElementById('schedule'));
+
     var socket = io.connect(window.location.origin);
 
     socket.on('schedule', function(data){
